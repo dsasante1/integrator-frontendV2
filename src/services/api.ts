a156f669be38cb1127ad46fe156233c3c6859259
ds asante <dsasante1@gmail.com>
2025-06-21 20:31:51 +0000

    fix: deployment ci

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { AxiosError, AxiosResponse } from 'axios';
 
-export const BASE_URL = 'https://integrator-api.verbgh.com/api/v1';
+export const BASE_URL = 'https://integrator-api.verbgh.com/integrator/api/v1';
 
 interface SnapshotDiffParams {
   pageSize?: number;
